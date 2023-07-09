226ba5d7f324b27b7731a7497c98bbe5677e56a4
smallfawn <860562056@QQ.COM>
2023-07-09 16:02:04 +0800

    增加防止休眠定时

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # glitch-ghproxy
  基于glitch提供的nodejs托管创建的github反代或加速服务
+ 教程:
+ 创建nodejs服务 导入本仓库
+ 配置保活定时 自己server.js 注释取消 然后修改地址
--- server.js
+++ server.js
@@ -8,18 +8,24 @@ app.get('/', (req, res) => {
   res.sendFile(filePath);
 });
 app.get('/raw/*', async (req, res) => {
-    const rawUrl = req.params[0];
-    console.log(rawUrl);
-    try {
-      const response = await axios.get(rawUrl, {headers: {'Accept-Encoding': 'identity'}});
-      res.send(response.data);
-      //console.log(response)
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'An error occurred' });
-    }
-    //res.send('URL参数获取成功');
-  });
+  const rawUrl = req.params[0];
+  console.log(rawUrl);
+  try {
+    const response = await axios.get(rawUrl, { headers: { 'Accept-Encoding': 'identity' } });
+    res.send(response.data);
+    //console.log(response)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+  //res.send('URL参数获取成功');
+});
+
+/*setInterval(async () => {
+  const response = await axios.get('https://这里写你的项目地址')
+  console.log(response.data);
+}, 300000);*///每5分钟请求一次自己的API保活防止休眠
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
